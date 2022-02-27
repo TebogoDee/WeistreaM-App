63361fa192ae2d236ac6a4f2f619806cdfd56dfb
Tee_Cee <40995805+TebogoDee@users.noreply.github.com>
2022-02-27 04:56:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+iimport { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.za.weistream.WeistreaM',
+  appName: 'WeistreaM',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
